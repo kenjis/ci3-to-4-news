ae122b5a4125802209b2ee3b46261bbfcee0d71c
kenjis <kenji.uui@gmail.com>
2021-02-19 15:13:00 +0900

    config: configure test database
    It is not good, but it is the same as development DB.

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -58,12 +58,12 @@ class Database extends Config
 	 */
 	public $tests = [
 		'DSN'      => '',
-		'hostname' => '127.0.0.1',
+		'hostname' => '',
 		'username' => '',
 		'password' => '',
-		'database' => ':memory:',
+		'database' => ROOTPATH . 'writable/database/sqlite-database.db',
 		'DBDriver' => 'SQLite3',
-		'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+		'DBPrefix' => '',
 		'pConnect' => false,
 		'DBDebug'  => (ENVIRONMENT !== 'production'),
 		'charset'  => 'utf8',
